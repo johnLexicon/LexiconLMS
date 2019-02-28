34c45e466b3203c97ca0850ab2488bb2eaa66649
John Lundgren <john.paul.lundgren@gmail.com>
2019-02-28 15:16:03 +0100

    Added View And ViewModel for Course

---
+++ LexiconLMS/ViewModels/AddCourseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconLMS.ViewModels
+{
+    public class AddCourseViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Course name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Display(Name = "Start date")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "End date")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+    }
+}
