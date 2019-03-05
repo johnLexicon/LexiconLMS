04bc7b1b0f955bbf0a422e61634f7486dc4c32c1
pp33gt <mapundi@outlook.com>
2019-03-05 16:28:55 +0100

    Generalize End Date Validator.

--- LexiconLMS/ViewModels/ModuleViewModel.cs
+++ LexiconLMS/ViewModels/ModuleViewModel.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace LexiconLMS.ViewModels
 {
-    public class ModuleViewModel
+    public class ModuleViewModel : IDateInterval
     {
         public int Id { get; set; }
 
