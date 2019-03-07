bf7a51352355a39f11d01899a86b97288badc69d
pp33gt <mapundi@outlook.com>
2019-03-07 12:36:45 +0100

    student start page to show courseInfo

---
+++ LexiconLMS/ViewModels/StudenCourseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconLMS.ViewModels
+{
+    public class StudenCourseViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Course name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Start date")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "End date")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Teacher email")]
+        public string TeacherEmail { get; set; }
+
+        public List<ModuleViewModel> Modules { get; set; }
+    }
+}
