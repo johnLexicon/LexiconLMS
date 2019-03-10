6f658222b30c4cfc45656541585eb82b3c98c4e5
mina fathi <fathimina64@gmail.com>
2019-03-10 15:29:44 +0100

    Show Details Of Course,Module,Activity in Students's StartPage

---
+++ LexiconLMS/Views/Student/Details.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace LexiconLMS.Views.Student
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class Details
+    {
+        private readonly RequestDelegate _next;
+
+        public Details(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class DetailsExtensions
+    {
+        public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Details>();
+        }
+    }
+}
