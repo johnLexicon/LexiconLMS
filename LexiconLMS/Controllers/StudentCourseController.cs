54d75fd954e73e64ea15be3f26c360f45fa0eaff
pp33gt <mapundi@outlook.com>
2019-03-18 12:20:52 +0100

    uncommit unused files

--- LexiconLMS/Controllers/StudentCourseController.cs
+++ LexiconLMS/Controllers/StudentCourseController.cs
@@ -1,76 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
-using Humanizer;
-using LexiconLMS.Data;
-using LexiconLMS.Models;
-using LexiconLMS.ViewModels;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using AutoMapper;
+//using Humanizer;
+//using LexiconLMS.Data;
+//using LexiconLMS.Models;
+//using LexiconLMS.ViewModels;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+//// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace LexiconLMS.Controllers
-{
-    [Authorize(Roles = "Student")]
-    public class StudentCourseController : Controller
-    {
-        private readonly LexiconLMSContext _context;
-        private readonly IMapper _mapper;
-        private readonly UserManager<User> _userManager;
+//namespace LexiconLMS.Controllers
+//{
+//    [Authorize(Roles = "Student")]
+//    public class StudentCourseController : Controller
+//    {
+//        private readonly LexiconLMSContext _context;
+//        private readonly IMapper _mapper;
+//        private readonly UserManager<User> _userManager;
 
-        public StudentCourseController(LexiconLMSContext context, IMapper mapper, UserManager<User> userManager)
-        {
-            _context = context;
-            _mapper = mapper;
-            _userManager = userManager;
+//        public StudentCourseController(LexiconLMSContext context, IMapper mapper, UserManager<User> userManager)
+//        {
+//            _context = context;
+//            _mapper = mapper;
+//            _userManager = userManager;
 
-        }
+//        }
 
 
-        public IActionResult Details(int id)
-        {
-            var course = _context.Courses.Include(c => c.Users).FirstOrDefault(c => c.Id == id);
+//        public IActionResult Details(int id)
+//        {
+//            var course = _context.Courses.Include(c => c.Users).FirstOrDefault(c => c.Id == id);
 
-            if (course is null)
-            {
-                return NotFound();
-            }
+//            if (course is null)
+//            {
+//                return NotFound();
+//            }
 
-            var viewModel = _mapper.Map<CourseDetailsViewModel>(course);
+//            var viewModel = _mapper.Map<CourseDetailsViewModel>(course);
 
-            var teacher = _userManager.GetUsersInRoleAsync("Teacher");
-            teacher.Wait();
+//            var teacher = _userManager.GetUsersInRoleAsync("Teacher");
+//            teacher.Wait();
 
-            var theTeacher = course.Users.Intersect(teacher.Result);
+//            var theTeacher = course.Users.Intersect(teacher.Result);
 
-            if (theTeacher.Count() < 1)
-            {
-                theTeacher = new List<User>() { new User() { Email = "not assigned" } };
-            }
+//            if (theTeacher.Count() < 1)
+//            {
+//                theTeacher = new List<User>() { new User() { Email = "not assigned" } };
+//            }
 
-            viewModel.TeacherEmail = theTeacher.FirstOrDefault().Email;
+//            viewModel.TeacherEmail = theTeacher.FirstOrDefault().Email;
 
-            viewModel.Documents = new List<DocumentListViewModel>();
-            //var documents = _context.CourseDocument.Where(d => d.CourseId == id).ToList();
-            //foreach (var doc in documents)
-            //{
-            //    var newDoc = _mapper.Map<DocumentListViewModel>(doc);
-            //    newDoc.Filezise = (doc.DocumentData.Length).Bytes().Humanize("#.#");
-            //    viewModel.Documents.Add(newDoc);
-            //}
+//            viewModel.Documents = new List<DocumentListViewModel>();
+//            //var documents = _context.CourseDocument.Where(d => d.CourseId == id).ToList();
+//            //foreach (var doc in documents)
+//            //{
+//            //    var newDoc = _mapper.Map<DocumentListViewModel>(doc);
+//            //    newDoc.Filezise = (doc.DocumentData.Length).Bytes().Humanize("#.#");
+//            //    viewModel.Documents.Add(newDoc);
+//            //}
 
-            //viewModel.Students = course.Users.Except(theTeacher);
+//            //viewModel.Students = course.Users.Except(theTeacher);
 
-            //viewModel.Modules = new List<ModuleViewModel>();
-            //var modules = _context.Modules.Where(a => a.CourseId == id).ToList();
-            //modules.ForEach(a => viewModel.Modules.Add(_mapper.Map<ModuleViewModel>(a)));
+//            //viewModel.Modules = new List<ModuleViewModel>();
+//            //var modules = _context.Modules.Where(a => a.CourseId == id).ToList();
+//            //modules.ForEach(a => viewModel.Modules.Add(_mapper.Map<ModuleViewModel>(a)));
 
-            return View(viewModel);
-        }
-    }
-}
+//            return View(viewModel);
+//        }
+//    }
+//}
--- LexiconLMS/Views/Student/Details.cs
+++ LexiconLMS/Views/Student/Details.cs
@@ -1,35 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Http;
 
-namespace LexiconLMS.Views.Student
-{
-    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
-    public class Details
-    {
-        private readonly RequestDelegate _next;
+//namespace LexiconLMS.Views.Student
+//{
+//    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+//    public class Details
+//    {
+//        private readonly RequestDelegate _next;
 
-        public Details(RequestDelegate next)
-        {
-            _next = next;
-        }
+//        public Details(RequestDelegate next)
+//        {
+//            _next = next;
+//        }
 
-        public Task Invoke(HttpContext httpContext)
-        {
+//        public Task Invoke(HttpContext httpContext)
+//        {
 
-            return _next(httpContext);
-        }
-    }
+//            return _next(httpContext);
+//        }
+//    }
 
-    // Extension method used to add the middleware to the HTTP request pipeline.
-    public static class DetailsExtensions
-    {
-        public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
-        {
-            return builder.UseMiddleware<Details>();
-        }
-    }
-}
+//    // Extension method used to add the middleware to the HTTP request pipeline.
+//    public static class DetailsExtensions
+//    {
+//        public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
+//        {
+//            return builder.UseMiddleware<Details>();
+//        }
+//    }
+//}
